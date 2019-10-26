d88cb40dadbd2efd246236e5cebd9b385a5d7a9b
AshuraZero <AshuraZero@users.noreply.github.com>
2019-10-27 01:58:06 +0800

    clean for classic

--- C/C_GetVersionNumber.lua
+++
@@ -1,6 +0,0 @@
-function C_GetVersionNumber()
-	-- Increment this when any C_ functions change.  
-	-- Useful for making sure users have a compatible version.
-
-	return 2;
-end
--- MozzFullWorldMap/localization.tc.lua
+++
@@ -1,8 +0,0 @@
-if (GetLocale()=="zhTW") then
-
--- Version : chinese ( by iWOW )
--- Last Update : 03/21/2005
-
-FULLWORLDMAP_SHOWALL	= "顯示未探索區域";
-
-end
